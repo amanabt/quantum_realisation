69ef7a9d1cdce184c9c338477ea0a95a420fd8a7
chimp <aman2official@gmail.com>
2016-03-02 00:19:28 +0530

    Final

--- code/library/include/finite_well.h
+++ code/library/include/finite_well.h
@@ -1,14 +1,15 @@
 #ifndef __FINITE_WELL__
 #define __FINITE_WELL__
 
+#include <iostream>
 
 class finite_well
 {
 public:
-	finite_well (long double a = 1.0,
-				 long double E = 5e-8,
-				 long double m = 1.0,
-				 long double dx = 1e-5) :
+	finite_well (double a = 1.0,
+				 double E = 5e-8,
+				 double m = 1.0,
+				 double dx = 1e-2) :
 				 _a (a),
 				 _E (E),
 				 _m (m),
@@ -16,34 +17,42 @@ public:
 			{}
 
 public:
-	auto start_front (long double slope_, long double psi) -> long double;
+	auto start_front (double slope_, double psi) -> double;
 
 public:
-	auto start_back  (long double slope_, long double psi) -> long double;
+	auto start_back  (double slope_, double psi) -> double;
 	
 public:
-	void set_Energy (long double E) { _E = E; }
+	void set_E (double E) { _E = E; }
+	
+public:
+	auto E (void) const -> double { return _E; }
+	
+public:
+	const auto ratio (void) const -> double
+	{ return _psi / _slope; }
+
+public:
+	bool is_eigen (double ratio) const 
+		{ std::cerr << ratio << ":" << (_psi / _slope) << ":" << (_psi / _slope) - ratio << std::endl;
+			return ((_psi / _slope) - ratio) < 0.1 && ((_psi / _slope) - ratio) > -0.1; }
 
 private:
-	auto second_derivative (long double V) const -> long double;
+	auto second_derivative (double V) const -> double;
 
 private:
-	void slope (long double V);
+	void slope (double V);
 
 private:
-	void write (long double x, long double psi) const;
+	void write (double x, double psi) const;
 
 private:
-	bool is_eigen (long double ratio) const 
-		{ return (_psi / _slope) == ratio; }
-
-private:
-	long double _a;
-	long double _E;
-	long double _m;
-	long double _dx;
-	long double _psi;
-	long double _slope;
+	double _a;
+	double _E;
+	double _m;
+	double _dx;
+	double _psi;
+	double _slope;
 };
 
 #endif
--- code/library/src/finite_well.cxx
+++ code/library/src/finite_well.cxx
@@ -8,27 +8,36 @@
 #include <thread>
 #include <chrono>
 #include <mpi.h>
+#include <memory>
+#include <unistd.h>
 
 static constexpr auto h = 1;
 static constexpr auto pi = 3.14159265359;
 static constexpr auto s = 2 * 1.0 / pow (h / (2 * pi), 2.0);
+
 void plot (void);
-auto finite_well::second_derivative (long double V) const -> long double
+
+auto finite_well::second_derivative (double V) const -> double
 {
 	return s * (V - _E) * _psi;
 }
 
-void finite_well::slope (long double V)
+// // // // // // // // // // // // // // // // // // // // // // // // 
+
+void finite_well::slope (double V)
 {
 	_slope = _slope + this -> second_derivative (V);
 }
 
-auto finite_well::start_front (long double slope_,
-							   long double psi) -> long double
+// // // // // // // // // // // // // // // // // // // // // // // // 
+
+auto finite_well::start_front (double slope_,
+							   double psi) -> double
 {
+	remove("test_front.txt");
 	_slope = slope_;
 	_psi = psi;
-	long double x = 0.0;
+	double x = 0.0;
 	std::ofstream ofs;
 	ofs.open ("test_front.txt", std::ofstream::out | std::ofstream::app);
 	while (x <= _a) {
@@ -40,17 +49,22 @@ auto finite_well::start_front (long double slope_,
 		slope (-1e-4);
 		x += _dx;
 	}
+
+	return _E;
 }
 
-auto finite_well::start_back (long double slope_,
-							  long double psi) -> long double
+// // // // // // // // // // // // // // // // // // // // // // // // 
+
+auto finite_well::start_back (double slope_,
+							  double psi) -> double
 {
+	remove ("test_back.txt");
 	_slope = slope_;
 	_psi = psi;
 	std::ofstream ofs;
 	ofs.open ("test_back.txt", std::ofstream::out | std::ofstream::app);
-	long double x = 3.0;
-	_dx *= -1;
+	double x = 2;
+	if (_dx > 0.0) _dx = _dx * -1;
 	while (x > _a) {
 		_psi = _psi + _slope * _dx;
 // 		write (x, _psi);
@@ -60,55 +74,97 @@ auto finite_well::start_back (long double slope_,
 		slope (0.0);
 		x += _dx;
 	}
+
+	return _E;
 }
-void finite_well::write (long double x, long double psi) const
+
+// // // // // // // // // // // // // // // // // // // // // // // // // 
+
+void finite_well::write (double x, double psi) const
 {
 	std::ofstream ofs;
 	ofs.open ("test.txt", std::ofstream::out | std::ofstream::app);
 	ofs << x << "," << psi << std::endl;
 }
 
+// // // // // // // // // // // // // // // // // // // // // // // // // 
+
+namespace utils {
 void plot (void)
 {
 	FILE * gnuplotPipe = popen ("gnuplot -persistent", "w");
-	std::string commandsForGnuplot[] = {"set datafile separator \",\"", "y = 0", "plot y"};
+	std::string commandsForGnuplot[] =
+	{ "set set datafile separator \",\"",
+	  "plot \"test_front.txt\" using 1:2 with l; replot \"test_back.txt\" using 1:2 with l"
+	};
 	fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[0].c_str());
 	fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[1].c_str());
 }
+};// namespace utils
 
 // // // // // // // // // // // // // // // // // // // // // // // // // // // // 
 
 int main (int argc, char **argv) 
 {
 	int rank_;
-	finite_well obj;
+	std::unique_ptr<finite_well> particle (new finite_well);
+	double dE = -1e-12, E;
+	double ratio;
+
+// 	auto i = 100;
 	try {
 		auto ec = MPI_Init (&argc, &argv);
 		if (ec != MPI_SUCCESS) throw ec;
 		std::cout << "Program Started" << std::endl;
 	}
-	
 	catch (int ec) {
 		MPI_Abort (MPI_COMM_WORLD, ec);
 		std::cout << "MPI_Init Failed with error code : " << ec << std::endl
 				  << "Aborting!" << std::endl;
 	}
-	
+
 	MPI_Comm_rank (MPI_COMM_WORLD, &rank_);
-	std::cerr << rank_ << std::endl;
 	
-	long double ratio;
 	if (rank_ == 0)
 	{
-		obj.start_front (1e-1, 0.0);
-		MPI_Recv(&ratio, 1, MPI_LONG_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		while (true){
+			particle->start_front (1e-3, 0.0);
+			MPI_Recv (&ratio, 1, MPI_LONG_DOUBLE, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+// 			std::cerr << ratio << std::endl;
+			E = particle->E() + dE;
+			if (!particle->is_eigen (ratio)) {
+				particle->set_E (E);
+				MPI_Send (&E, 1, MPI_LONG_DOUBLE, 1, 1, MPI_COMM_WORLD);
+			}
+			
+			else {
+				E = 0.0;
+				MPI_Send (&E , 1, MPI_LONG_DOUBLE, 1, 1, MPI_COMM_WORLD);
+				break;
+			}
+// 			std::cerr << rank_ << std::endl;
+// 			--i;
+		}
+		
 	}
 
 	else 
-		if (rank_ == 1)
-			obj.start_back (1e-10, 1e-3);
+		if (rank_ == 1) {
+			while (true) {
+				particle->start_back (1e-10, 1e-10);
+				ratio = particle->ratio();
+// 				std::cerr << ratio << std::endl;
+				MPI_Send (&ratio, 1, MPI_LONG_DOUBLE, 0, 0, MPI_COMM_WORLD);
+				MPI_Recv (&E, 1, MPI_LONG_DOUBLE, 0, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				
+				if (E == 0.0) break;
+				else particle->set_E (E);
+// 				std::cerr << rank_ << std::endl;
+// 				--i;
+			}
+		}
 	
-	else { 
+	else {
 		MPI_Finalize();
 		return 0;
 	}
